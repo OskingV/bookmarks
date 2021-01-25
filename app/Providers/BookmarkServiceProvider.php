3bee50e72e7608f504ce4bbe9534caef339b147e
Vlad Oskin <oskinvv95@gmail.com>
2021-01-25 16:40:30 +0700

    Add method to store bookmark

---
+++ app/Providers/BookmarkServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Site\Contracts\Parser;
+use App\Services\Site\CrawlerParser;
+use Illuminate\Support\ServiceProvider;
+
+class BookmarkServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(Parser::class, CrawlerParser::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
